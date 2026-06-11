bd22e2bc07476f5a3c99688197c0beedc4ad0da2
liminallogs <liminallogs@proton.me>
2026-06-10 22:36:58 -0400

    chore: Tweaks for css to compile alongside latex/katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import rehypeKatex from 'rehype-katex';
 export default defineConfig({
     integrations: [astroImageTools, mdx()],
     site: 'https://lorenzoevans.github.io/',
-    base: process.env.NODE_ENV == 'production' ? '/lorenzoevans.github.io' : '/',
     markdown: {
         remarkPlugins: [remarkMath],
         rehypePlugins: [rehypeKatex],
